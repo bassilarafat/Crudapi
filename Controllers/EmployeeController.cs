e45c2f5aec2043646ac5d628fe324a129781417b
basel arafat <basel arafat@BASEL>
2022-01-09 06:37:12 +0200

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using Crudapi.Models;
+using Crudapi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crudapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //CRUD
+        [HttpGet]
+        public ActionResult<List<Employee>> GetEmployee() => EmployeeService.GetEmployees();
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetEmploye(int id)
+        {
+            var emp = EmployeeService.GetEmployee(id);
+            if (emp == null)
+                return NotFound();
+            return emp;
+        }
+        [HttpPost]
+        public ActionResult CreateEmployee(Employee employee)
+        {
+            EmployeeService.Add(employee);
+            return CreatedAtAction(nameof(CreateEmployee), new { id = employee.Id }, employee);     //to return 201 response code
+        }
+
+        [HttpPut]
+        public ActionResult Update(Employee employee)
+        {
+
+            EmployeeService.Update(employee);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id )
+        {
+            var emp = EmployeeService.GetEmployee(id);
+            if (emp is null)
+                return NotFound();
+            EmployeeService.Delete(id);
+            return NoContent();
+        }
+        
+        
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,56 @@
+﻿using Crudapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crudapi.Services
+{
+    public class EmployeeService
+    {
+
+        //to initialize the list we have do that in ctor  ???
+
+        static List<Employee> employeesList { get; }
+        static int NextEmpId = 3;
+        static EmployeeService()
+        {
+            employeesList = new List<Employee>()
+            {
+                new Employee{Id=1,Name="ziad",position="Manager",Salary=8800},
+                new Employee{Id=1,Name="Ahmed",position="Ceo",Salary=14000},
+            };
+        }
+        
+        //Create my operations here not Controller for SOC
+
+        public static List<Employee> GetEmployees()=> employeesList;  //fun ti get list od employees 
+
+        public static Employee GetEmployee(int id) => employeesList.SingleOrDefault(e => e.Id == id);
+
+        public static void Add(Employee employee)
+        {
+            employee.Id = NextEmpId++;     //id =3 then plus 
+            employeesList.Add(employee);
+        }
+        public static  void Update(Employee employee)
+        {
+
+            var index = employeesList.FindIndex(e=>e.Id==employee.Id);        //LINQ and lambda exp
+            if (index == -1)
+                return;
+            employeesList[index] = employee;
+        }
+
+        public static void Delete(int id)
+        {
+            var emp = GetEmployee(id);
+            if (emp == null)
+                return;
+            employeesList.Remove(emp);
+        }
+
+
+    }
+   
+}
